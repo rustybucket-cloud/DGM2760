15824b7b74de3379722287f608cce230248b4f53
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2021-09-13 14:12:49 -0600

    added HTML structure and header styling

---
+++ Unit03/script.js
@@ -0,0 +1,54 @@
+//pick a random number between 1 and 16
+let correctNumber = Math.floor(Math.random() * 15) + 1
+console.log(`The correct number is ${correctNumber}`) 
+
+let guessed = false
+let totalGuesses = 0
+let gamerGuess = 0
+
+function evalGuess() {
+   totalGuesses += 1 //increment total guesses
+   gamerGuess = document.querySelector('#guess').value
+   const feedback = document.querySelector('#feedback')
+
+   if (gamerGuess == correctNumber) {
+       feedback.innerText = 'You win!'
+       giveAward(totalGuesses)
+   } 
+   else if(gamerGuess > correctNumber && gamerGuess < 16) {
+        feedback.innerText = 'Too high, try again'
+   }
+   else if (gamerGuess < correctNumber && gamerGuess > 0) {
+       feedback.innerText = 'Too low, try again'
+   }
+   else {
+       feedback.innerText = 'Please select a number between 1 and 15 and try again'
+   }
+
+   document.querySelector('#attempts').innerText = totalGuesses
+}
+
+function giveAward(guesses) {
+    let imagePath = '#'
+    switch(guesses) {
+        case 1:
+        case 2:
+        case 3:
+            console.log('Blue ribbon')
+            break
+        case 4: 
+        case 5:
+        case 6:
+            console.log('Red Ribbon')
+            break
+        case 7:
+        case 8:
+        case 9:
+            console.log("Yellow Ribbon")
+            break
+    }
+
+    const awardImage = document.createElement('img') //creates <img>
+    const ribbon = document.querySelector('#ribbon')
+    ribbon.appendChild(awardImage);
+}
